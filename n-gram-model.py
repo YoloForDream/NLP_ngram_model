54c02ca1d850e82d5761c51484bb7443e1c2cf4b
YoloForDream <34928079+YoloForDream@users.noreply.github.com>
2020-08-28 07:41:48 +0800

    Update n-gram-model.py

--- n-gram-model.py
+++ n-gram-model.py
@@ -9,7 +9,8 @@ import re
 from collections import Counter, namedtuple
 from nltk.util import ngrams
 import nltk
- 
+
+
 def read_data(path):
     file = open(path, 'r', encoding='utf-8').read()
     pattern = re.compile(r'<content>(.*?)</content>', re.S)
@@ -17,7 +18,7 @@ def read_data(path):
     contents = [i for i in contents if i != '']
     return contents
 
-def tuple_edit(t):
+def tuple_edit(t):# replace (to,want) with (to,want.)
     l = len(t)
     t1 =()
     last_value = t[l-1]+'.'
@@ -26,32 +27,31 @@ def tuple_edit(t):
     t2 = tuple(last_value_list)
     t1 = t1 +t[0:l-1]+t2
     return t1
-   
+
 def ngram_training(document, N,key):
     total_grams = []
     words = []
     for doc in document:
         split_words = list(doc.split(' '))
         # molecular
-        [total_grams.append(tuple(split_words[i: i+N])) for i in range(len(split_words)-N+1)]
+        [total_grams.append(tuple(split_words[i: i+N])) for i in range(len(split_words)-N+1)]#if N is 2 it count(A,B)
         # denominator
-        [words.append(tuple(split_words[i:i+N-1])) for i in range(len(split_words)-N+2)]
+        [words.append(tuple(split_words[i:i+N-1])) for i in range(len(split_words)-N+2)]# if N is 2 it count(A)
     total_word_counter = Counter(total_grams)
     word_counter = Counter(words)
-    a = total_word_counter[key]
-    b = word_counter[key[:N - 1]]  
-    c = total_word_counter[tuple_edit(key)]
-    if a ==0 and c!=0:
-            a = c
-    if a!=0 and b!=0:
-             next_word_prob = a/b
-    elif a==0 and b!=0:#bakeoff
-             next_word_prob = (a+1)/(b+1)
-    elif a==0 and b==0:
-             next_word_prob = 1
+    count_molecular = total_word_counter[key]#  if P(B|A) = count(A,B)/count(A) it is the value of count(A,B)
+    count_denominator = word_counter[key[:N - 1]]  # count(A) value
+    count_molecular_change = total_word_counter[tuple_edit(key)]#Sample:if (to,want) not in corpus try to check (to ,want.)
+    if count_molecular == 0 and count_molecular_change !=0:
+            count_molecular = count_molecular_change
+    if count_molecular != 0 and  count_denominator!= 0:
+              next_word_prob = count_molecular/count_denominator
+    elif count_molecular == 0 and count_denominator != 0:#bakeoff
+              next_word_prob = (count_molecular+1)/(count_denominator+1)
+    elif count_molecular == 0 and count_denominator == 0:
+              next_word_prob = 1
     return next_word_prob
-
-
+  
 def precision(t,n,data):
     ngram_probability =[]
     for w in t:
@@ -61,13 +61,11 @@ def precision(t,n,data):
     t = Sen_possibility(ngram_probability)
     return t
     
-
 def Sen_possibility(list_P):
     P = 1
     for p in list_P:
         P *= p
     return P
-
 
 if __name__ == '__main__':
     corpus_list =['t_1.txt','t_2.txt','t_3.txt','t_4.txt','t_5.txt',
@@ -75,16 +73,16 @@ if __name__ == '__main__':
     user_input = input("Inpurt your word:")
     unigrams = nltk.word_tokenize(user_input)
     n = int(input('Input the n value for n-grams:'))
-    t = list(ngrams(unigrams, n))
-    print(t)
+    input_ngram = list(ngrams(unigrams, n))
+    print(input_ngram)
     probability_collection = []
-    for c in corpus_list:    
-        c = 'corpus/'+c
-        data = read_data(c)
-        p = precision(t,n,data)
+    for corpus in corpus_list:    #The probability of a Sent in a 10-word library is calculated
+        corpus = 'corpus/'+corpus
+        data = read_data(corpus)
+        p = precision(input_ngram,n,data)
         probability_collection.append(p)
     print(probability_collection)
-    for p in probability_collection:
+    for p in probability_collection:#Calculate the average of the 10 probability values
         total_p  = p + p 
     expect = total_p/10
     print(expect)
