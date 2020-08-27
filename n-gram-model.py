5aedc3a385bb1db1799975524fd5cf4880f118cb
YoloForDream <34928079+YoloForDream@users.noreply.github.com>
2020-08-27 08:55:52 +0800

    Add files via upload

---
+++ n-gram-model.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 18 20:56:22 2020
+
+@author: wangwei
+"""
+import re
+from collections import Counter, namedtuple
+from nltk.util import ngrams
+import nltk
+# import threading
+# from nltk.tokenize import word_tokenize  
+
+def read_data(path):
+    file = open(path, 'r', encoding='utf-8').read()
+    pattern = re.compile(r'<content>(.*?)</content>', re.S)
+    contents = pattern.findall(file)
+    contents = [i for i in contents if i != '']
+    return contents
+
+def tuple_edit(t):
+    l = len(t)
+    t1 =()
+    last_value = t[l-1]+'.'
+    last_value_list = []
+    last_value_list.append(last_value)
+    t2 = tuple(last_value_list)
+    t1 = t1 +t[0:l-1]+t2
+    return t1
+
+def ngram_training(document, N,key):
+    total_grams = []
+    words = []
+    for doc in document:
+        split_words = list(doc.split(' '))
+        # molecular
+        [total_grams.append(tuple(split_words[i: i+N])) for i in range(len(split_words)-N+1)]
+        # denominator
+        [words.append(tuple(split_words[i:i+N-1])) for i in range(len(split_words)-N+2)]
+    total_word_counter = Counter(total_grams)
+    word_counter = Counter(words)
+    a = total_word_counter[key]
+    b = word_counter[key[:N - 1]]  
+    c = total_word_counter[tuple_edit(key)]
+    if a ==0 and c!=0:
+            a = c
+    if a!=0 and b!=0:
+             next_word_prob = a/b
+    elif a==0 and b!=0:#bakeoff
+             next_word_prob = (a+1)/(b+1)
+    elif a==0 and b==0:
+             next_word_prob = 1
+    return next_word_prob
+
+
+def precision(t,n,data):
+    #data = read_data('t_2.txt')
+    # ngram_probability =[]
+    # t = []
+    # t = input_list
+    # data = []
+    # # data = txt_data
+    # n = N
+    ngram_probability =[]
+    for w in t:
+        p= ngram_training(data,n,w)
+        ngram_probability.append(p)
+    print(ngram_probability)
+    t = Sen_possibility(ngram_probability)
+    return t
+    
+
+def Sen_possibility(list_P):
+    P = 1
+    for p in list_P:
+        P *= p
+    return P
+
+
+if __name__ == '__main__':
+    corpus_list =['t_1.txt','t_2.txt','t_3.txt','t_4.txt','t_5.txt',
+                't_6.txt','t_7.txt','t_8.txt','t_9.txt','t_10.txt']
+    # data = read_data('corpus/'+corpus_list[0])
+    # c = 'corpus/'+'t_1.txt'
+    # data = read_data(c)
+    # print(data)
+    user_input = input("Inpurt your word:")
+    unigrams = nltk.word_tokenize(user_input)
+    n = int(input('Input the n value for n-grams:'))
+    t = list(ngrams(unigrams, n))
+    print(t)
+    probability_collection = []
+    for c in corpus_list:    
+        c = 'corpus/'+c
+        data = read_data(c)
+        p = precision(t,n,data)
+        probability_collection.append(p)
+    print(probability_collection)
+    for p in probability_collection:
+        total_p  = p + p 
+    expect = total_p/10
+    print(expect)
+    
